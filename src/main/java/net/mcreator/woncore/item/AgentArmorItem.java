1cf5733cde41383ce9ebe94907f889a835044433
Lucass27400 <Lucas@MSI.home>
2023-11-29 22:23:11 +0100

    Ajout d'item de craft pour armes Traduction

--- src/main/java/net/mcreator/woncore/item/AgentArmorItem.java
+++ src/main/java/net/mcreator/woncore/item/AgentArmorItem.java
@@ -1,10 +1,32 @@
 
 package net.mcreator.woncore.item;
 
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.client.IItemRenderProperties;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.woncore.init.WoncoreModTabs;
+import net.mcreator.woncore.client.model.ModelTenue_Agent;
+
+import java.util.Map;
+import java.util.Collections;
 
 public abstract class AgentArmorItem extends ArmorItem {
-
 	public AgentArmorItem(EquipmentSlot slot, Item.Properties properties) {
 		super(new ArmorMaterial() {
 			@Override
@@ -50,7 +72,6 @@ public abstract class AgentArmorItem extends ArmorItem {
 	}
 
 	public static class Helmet extends AgentArmorItem {
-
 		public Helmet() {
 			super(EquipmentSlot.HEAD, new Item.Properties().tab(WoncoreModTabs.TAB_COMBAT));
 		}
@@ -76,11 +97,9 @@ public abstract class AgentArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "woncore:textures/entities/tenu_agent_texture.png";
 		}
-
 	}
 
 	public static class Chestplate extends AgentArmorItem {
-
 		public Chestplate() {
 			super(EquipmentSlot.CHEST, new Item.Properties().tab(WoncoreModTabs.TAB_COMBAT));
 		}
@@ -106,11 +125,9 @@ public abstract class AgentArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "woncore:textures/entities/tenu_agent_texture.png";
 		}
-
 	}
 
 	public static class Leggings extends AgentArmorItem {
-
 		public Leggings() {
 			super(EquipmentSlot.LEGS, new Item.Properties().tab(WoncoreModTabs.TAB_COMBAT));
 		}
@@ -137,11 +154,9 @@ public abstract class AgentArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "woncore:textures/entities/tenu_agent_texture.png";
 		}
-
 	}
 
 	public static class Boots extends AgentArmorItem {
-
 		public Boots() {
 			super(EquipmentSlot.FEET, new Item.Properties().tab(WoncoreModTabs.TAB_COMBAT));
 		}
@@ -168,7 +183,5 @@ public abstract class AgentArmorItem extends ArmorItem {
 		public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
 			return "woncore:textures/entities/tenu_agent_texture.png";
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/woncore/item/ReceiverItem.java
+++ src/main/java/net/mcreator/woncore/item/ReceiverItem.java
@@ -1,13 +1,13 @@
 
 package net.mcreator.woncore.item;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import javax.annotation.Nullable;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+import net.mcreator.woncore.init.WoncoreModTabs;
 
 public class ReceiverItem extends Item {
-
 	public ReceiverItem() {
 		super(new Item.Properties().tab(WoncoreModTabs.TAB_ITEMS).stacksTo(64).rarity(Rarity.COMMON));
 	}
-
 }
