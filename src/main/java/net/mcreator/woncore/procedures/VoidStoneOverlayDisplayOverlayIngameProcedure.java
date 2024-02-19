9e48f8e34da8ecfd9068fb87778e3bb411ce5c62
Lucass27400 <lucas.rouzee@orange.fr>
2024-02-19 23:42:26 +0100

    Edit voidstone

--- src/main/java/net/mcreator/woncore/procedures/VoidStoneOverlayDisplayOverlayIngameProcedure.java
+++ src/main/java/net/mcreator/woncore/procedures/VoidStoneOverlayDisplayOverlayIngameProcedure.java
@@ -1,5 +1,6 @@
 package net.mcreator.woncore.procedures;
 
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
@@ -11,6 +12,14 @@ public class VoidStoneOverlayDisplayOverlayIngameProcedure {
 		if (entity == null)
 			return false;
 		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(WoncoreModItems.VOID_STONE.get())) : false) {
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = new ItemStack(Blocks.STONE);
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = new ItemStack(Blocks.COBBLESTONE);
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+			}
 			return true;
 		}
 		return false;
